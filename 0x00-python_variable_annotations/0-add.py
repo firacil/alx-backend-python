8289c35623db27465dd58a4acd85258705ba7f61
Firaol Mk <firaolmekbib511@gmail.com>
2024-08-02 13:26:48 -0700

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """function to add two numbers using variable annotation"""
 
+
 def add(a: float, b: float) -> float:
+    """function to add two numbers"""
     return a + b
