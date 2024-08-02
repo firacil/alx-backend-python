84d03015c775ff944efeaec7482fd8423285aa61
Firaol Mk <firaolmekbib511@gmail.com>
2024-08-02 13:58:05 -0700

    sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module to return return sum of list"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """take a list and return their sum"""
+    return sum(input_list)
