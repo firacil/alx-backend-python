16ed698199fbbf6af97d149679905803e75ab7e8
Firaol Mk <firaolmekbib511@gmail.com>
2024-08-02 13:44:58 -0700

    updated

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module to convert float into string representation"""
+
+
+def to_str(n: float) -> str:
+    """returns string representation of the float"""
+    return str(n)
