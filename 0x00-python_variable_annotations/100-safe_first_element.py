caa8800243f60b3ce56b236151633578f97256e6
Firaol Mk <firaolmekbib511@gmail.com>
2024-08-02 14:40:10 -0700

    safe_first_element

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""module to augment the following with
+   correct duck-typed annotations
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
