eb5f194ae347b85c1364e0888e8e7d8104aed214
Firaol Mk <firaolmekbib511@gmail.com>
2024-08-02 13:51:08 -0700

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module define and annotate variables to the given value"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
