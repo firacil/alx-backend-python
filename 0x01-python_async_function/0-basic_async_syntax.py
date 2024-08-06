e21b4bd55a3a881de4c9c585f356e3b8c37f239d
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-08-06 18:52:34 +0000

    basic async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""code to use async and await"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """
+        function to await between 0 and max_delay
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
